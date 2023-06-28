bcdee78fda196cb2260b4da11a45ea95d180aa7d
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-06-28 01:08:03 -0300

    Agrego script 481 con resultados del script 321

---
+++ src/rpart/321_resultado_rpart_BO.R
@@ -0,0 +1,7 @@
+library(readr)
+Resultado_z_321 <- read_delim("Resultado_z_321.txt", 
+                              delim = "\t", escape_double = FALSE, 
+                              trim_ws = TRUE)
+View(Resultado_z_321)
+
+

677c9a84aa505b387831ea2753d31a219af2ae54
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-07-06 23:35:06 -0300

    Agrego scripts prueba 01

--- src/workflow-inicial/experimento04_grupo_A/601_RUN_correr_workflow_prueba01.R
+++ src/workflow-inicial/experimento04_grupo_A/601_RUN_correr_workflow_prueba01.R
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/611_CA_reparar_dataset_EXP04_A.r")
-source("~/labo2023r/src/workflow-inicial/621_DR_corregir_drifting_EXP04_A.r")
-source("~/labo2023r/src/workflow-inicial/631_FE_historia_EXP04_A_prueba01.r")
-source("~/labo2023r/src/workflow-inicial/641_TS_training_strategy_EXP04_A_prueba01.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/611_CA_reparar_dataset_EXP04_A.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/621_DR_corregir_drifting_EXP04_A.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/631_FE_historia_EXP04_A_prueba01.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/641_TS_training_strategy_EXP04_A_prueba01.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/651_HT_lightgbm_EXP04_A_prueba01.r")
-source("~/labo2023r/src/workflow-inicial/661_ZZ_final_EXP04_A_prueba01.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/651_HT_lightgbm_EXP04_A_prueba01.r")
+source("~/labo2023r/src/workflow-inicial/experimento04_grupo_A/661_ZZ_final_EXP04_A_prueba01.r")

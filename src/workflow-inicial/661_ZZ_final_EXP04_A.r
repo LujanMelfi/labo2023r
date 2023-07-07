0a69e5907811f89bd19eccbd66aedda120872be9
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-07-06 21:19:42 -0300

    Agrego script exp 04

--- src/workflow-inicial/661_ZZ_final_EXP04_A.r
+++ src/workflow-inicial/661_ZZ_final_EXP04_A.r
@@ -35,15 +35,15 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ6610"
-PARAM$exp_input <- "HT6510"
+PARAM$experimento <- "ZZ6610_EXP04_A"
+PARAM$exp_input <- "HT6510_EXP04_A"
 
 # Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
 # Que modelos quiero, segun su posicion en el ranking e la Bayesian Optimizacion, ordenado por ganancia descendente
 PARAM$modelos_rank <- c(1)
 
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(591067, 157991, 689987, 136999, 366467)
 
 PARAM$kaggle$envios_desde <- 9500L
 PARAM$kaggle$envios_hasta <- 11500L

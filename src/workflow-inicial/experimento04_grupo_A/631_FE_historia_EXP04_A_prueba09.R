449bfebdc738b35d0d13c67fb57fe742d1098390
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-07-13 23:40:10 -0300

    Agrego scripts de la prueba 09

--- src/workflow-inicial/experimento04_grupo_A/631_FE_historia_EXP04_A_prueba09.R
+++ src/workflow-inicial/experimento04_grupo_A/631_FE_historia_EXP04_A_prueba09.R
@@ -46,7 +46,7 @@ PARAM$Tendencias2$ratiomax <- FALSE
 
 PARAM$RandomForest$run <- TRUE # FALSE PARA PRUEBA 01, LAS DEMÁS VA TRUE
 PARAM$RandomForest$num.trees <- 800
-PARAM$RandomForest$max.depth <- 5
+PARAM$RandomForest$max.depth <- 4
 PARAM$RandomForest$min.node.size <- 200
 PARAM$RandomForest$mtry <- 40
 PARAM$RandomForest$semilla <- 980717 # cambiar por la propia semilla

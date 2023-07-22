ffcc5a195305830ea36c276bbeae053211bf76a3
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-07-22 17:46:16 -0300

    Agrego una búsqueda dentro del ranking para semillerío 3

--- src/Workflow_semillerio_entrega_final/Analisis_BOlog_HT7510.R
+++ src/Workflow_semillerio_entrega_final/Analisis_BOlog_HT7510.R
@@ -4,8 +4,8 @@ BO_log_HT7510 <- read_delim("src/Workflow_semillerio_entrega_final/BO_log_HT7510
                             trim_ws = TRUE)
 View(BO_log_HT7510)
 
-#--------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 
 library(dplyr)
 
@@ -26,3 +26,15 @@ minimo
 
 posicion_ranking <- which(BO_gcia_descendente$ganancia == minimo)
 posicion_ranking
+
+#-----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Qué puesto ocupa la ganancia = 63.872.697
+
+iteracion <- 11 # lo busco por N° de iteración dado que no reconoce la ganancia por los decimales, solución más simple
+
+posicion_ranking <- which(BO_gcia_descendente$iteracion_bayesiana == 11)
+posicion_ranking
+
+# Resultado = 64 voy a usar ésta en semillerío 3

7ce0329b65998cdb0c921e635bdecb91a3df6f7e
LujanMelfi <lmelfi@mail.austral.edu.ar>
2023-07-21 23:18:15 -0300

    Agrego análisis de la BO de HT7510

--- src/Workflow_semillerio_entrega_final/Analisis_BOlog_HT7510.R
+++ src/Workflow_semillerio_entrega_final/Analisis_BOlog_HT7510.R
@@ -4,3 +4,25 @@ BO_log_HT7510 <- read_delim("src/Workflow_semillerio_entrega_final/BO_log_HT7510
                             trim_ws = TRUE)
 View(BO_log_HT7510)
 
+#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------
+
+library(dplyr)
+
+BO_gcia_descendente <- BO_log_HT7510 %>% arrange(desc(ganancia))
+
+# Ganancia más alta obtenida
+
+maximo <- max(BO_gcia_descendente$ganancia)
+maximo
+
+posicion_ranking <- which(BO_gcia_descendente$ganancia == maximo)
+posicion_ranking
+
+# Ganancia mínima obtenida 
+
+minimo <- min(BO_gcia_descendente$ganancia)
+minimo
+
+posicion_ranking <- which(BO_gcia_descendente$ganancia == minimo)
+posicion_ranking
